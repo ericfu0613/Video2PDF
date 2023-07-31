a25e35da7f8bb22668f387e90ee4bda7ec36fce5
ericfu0613 <132423425+ericfu0613@users.noreply.github.com>
2023-07-31 16:28:43 +0800

    Update download_video.py

--- download_video.py
+++ download_video.py
@@ -1,35 +1,28 @@
 import subprocess
+import os
+import multiprocessing
+
 
 def download_video(url):
     video_format = "18"  # This will download mp4 video in 640x360 resolution
-    output_template = "%(title)s.%(ext)s"  # This will name the video file as "title.mp4"
-
-    # Build the yt-dlp command
-    command = [
-        "yt-dlp",
-        "-f", video_format,
-        "-o", output_template,
-        url,
-    ]
-
-    # Execute the yt-dlp command
-    subprocess.run(command, check=True)
-
-    # Get the video title
-    video_title = subprocess.check_output(["yt-dlp", "--get-filename", "-o", "%(title)s", url])
+	@@ -20,16 +23,20 @@ def download_video(url):
     video_title = video_title.decode("utf-8").strip()
     video_filename = f"{video_title}.mp4"
 
+    print(video_filename)
     # Call the generate_srt function
     generate_srt(video_filename)
+
 
     return video_filename
 
 def generate_srt(video_filename):
     # Generate srt using whisper-ctranslate2
+    num_cores = multiprocessing.cpu_count()
+    print("This notebook has access to {} cores".format(num_cores))
     command_srt = [
         "whisper-ctranslate2",
-        "--threads", "8",
+        "--threads", str(num_cores) ,
         "--output_format", "srt",
         video_filename,
     ]
